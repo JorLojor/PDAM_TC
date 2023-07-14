800affeb8bb9973742776bca7a88290942bbf6bc
ahlilfikri <ahlilfikri49@gmail.com>
2023-07-14 19:37:59 +0700

    fixing models

--- models/kelas.js
+++ models/kelas.js
@@ -2,6 +2,7 @@ const moongose = require('mongoose');
 const {Schema} = moongose;
 
 const materi = require('./materi');
+// const peserta = require('./peserta');
 
 
 const kelasSchema = new Schema({
@@ -11,8 +12,8 @@ const kelasSchema = new Schema({
     kapasitasPeserta : {type: Number, required: true},
     description : {type: String, required: true},
     methods : {type: String, required: true}, //online,offline, onlineMeeting
-    materi : [materi], //refrensi ke schema materi
-    peserta : [peserta], //refrensi ke schema user dengan role 3 atau peserta hanya untuk 
+    materi : {type: Schema.Types.ObjectId, ref: 'materi', required: false}, //refrensi ke schema materi
+    peserta : {type: Schema.Types.ObjectId, ref: 'peserta', required: false}, //refrensi ke schema user dengan role 3 atau peserta hanya untuk 
     instruktur : {type: Schema.Types.ObjectId, ref: 'instruktur', required: false}, //refrensi ke schema user dengan role 2 atau instruktur
     kodeNotaDinas: {type: String, required: false}, //refrensi ke schema
 });
