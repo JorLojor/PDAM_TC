8394d3b9c4009547eba3981a261894f523855d81
ahlilfikri <ahlilfikri49@gmail.com>
2023-07-18 06:46:19 +0700

    feat/auth : fixing authorization

--- routers/jadwal.js
+++ routers/jadwal.js
@@ -4,9 +4,9 @@ const auth = require('../middleware/auth');
 const Controller = require('../controllers/jadwal');
 
 router.get('/', Controller.getJadwal);
-router.post('/', Controller.createJadwal);
-router.put('/:id', Controller.updateJadwal);
-router.delete('/:id', Controller.deleteJadwal);
+router.post('/', auth.admin,auth.instruktur,Controller.createJadwal);
+router.put('/:id', auth.admin,auth.instruktur,Controller.updateJadwal);
+router.delete('/:id', auth.admin,Controller.deleteJadwal);
 
 
 module.exports = router;
