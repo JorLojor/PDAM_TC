02f798da973f926e2a7e725432195d596225754c
ahlilfikri <ahlilfikri49@gmail.com>
2023-07-18 06:41:59 +0700

    feat/auth : fixing authorization

--- routers/kelas.js
+++ routers/kelas.js
@@ -5,9 +5,9 @@ const Controller = require('../controllers/kelas');
 
 router.get('/', Controller.getAllKelas);
 router.get('/:id', Controller.getOneKelas);
-router.post('/', Controller.createKelas);
-router.put('/:id', Controller.updateKelas);
-router.delete('/:id', Controller.deleteKelas);
+router.post('/', auth.admin,Controller.createKelas);
+router.put('/:id', auth.admin,Controller.updateKelas);
+router.delete('/:id', auth.admin,Controller.deleteKelas);
 
 
 module.exports = router;
