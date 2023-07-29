235b5ae758f130d14ef08a13b1519911251efa26
ahlilfikri <ahlilfikri49@gmail.com>
2023-07-29 18:21:11 +0700

    feat/user adding createUser,getstatus,updatestatus in user

--- routers/user.js
+++ routers/user.js
@@ -8,13 +8,13 @@ const userController = require('../controllers/user');
 router.get('/all' ,userController.getAllUser);
 router.get('/:id', auth.admin ,userController.getSingleUser);
 
-router.get('/',userController.getStatusPendingUser); // get status pending userå
+router.get('/',auth.admin,userController.getStatusPendingUser); // get status pending userå
 
 router.post('/create', auth.admin ,userController.createUser);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.put('/:id',auth.admin , userController.updateUser);
-router.put('/status/:id', userController.updateStatusUser);
+router.put('/status/:id',auth.admin , userController.updateStatusUser);
 router.delete('/:id',auth.admin , userController.deleteUser);
 
 
