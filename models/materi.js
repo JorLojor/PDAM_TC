d9d7849fa8cebcfeb2732e4f15f5e9cabc391b21
ahlilfikri <ahlilfikri49@gmail.com>
2023-08-01 15:22:13 +0700

    feat/materi fixing materi and tugas

--- models/materi.js
+++ models/materi.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const {Schema} = mongoose;
+const itemsMateri = require('./itemsMateri');
 
 const materiSchema = new Schema({
     kodeMateri: {type: String, required: true},
-    nama : {type: String, required: true},
+    section : {type: String, required: true},
     description : {type: String, required: true},
-    type : {type: String, required: true}, //file / video
-    source: {type: String, required: true}, // yputube gdrive
-    tugas : {type: mongoose.Schema.Types.ObjectId, ref: 'tugas'},
-    
+    items : {type: [itemsMateri]}
 },{ timestamps: true });
-
+    
 module.exports = mongoose.model('Materi', materiSchema);
 
 // Path: models/materi.js
