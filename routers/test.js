b09edbd3c7219c6336c75acc7492a8fb57c26c0f
Sahrul Ramdan <sahrulramdan.75@gmail.com>
2023-08-22 09:32:46 +0700

    fix(materi): config multer

--- routers/test.js
+++ routers/test.js
@@ -4,12 +4,26 @@ const testRouter = express.Router();
 const auth = require("../middleware/auth");
 const Controller = require("../controllers/test");
 
+const path = require('path')
+const multer = require('multer')
+
+const today = new Date().toISOString().slice(0,10)
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '..', 'upload', 'kelas-featured-image',today),
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+
+  const upload = multer({ storage })
+
 //user
 // testRouter.get("/", auth.user, Controller.getTest);
 
 //buat yang bisa post test
 // testRouter.get("/fu/:id", auth.user, auth.instruktur, Controller.followUp);
 // testRouter.get("/room/:id", auth.user, auth.instruktur, Controller.getRoom);
-testRouter.post("/store", auth.user, auth.instruktur, Controller.store);
+testRouter.post("/store", auth.user, auth.instruktur,upload.array('images'), Controller.store);
 
 module.exports = testRouter;
