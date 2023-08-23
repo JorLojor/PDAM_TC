330a29a1e7a07e0a014a17cd52c145c7066f14f7
Sahrul Ramdan <sahrulramdan.75@gmail.com>
2023-08-23 17:09:18 +0700

    fixing chats

--- routers/room.js
+++ routers/room.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const Controller = require("../controllers/room");
 
-router.get("/", auth.user, auth.admin, Controller.index);
+router.get("/", auth.user, Controller.index);
 router.post("/", auth.user, auth.onlyStudent, Controller.store);
 
 module.exports = router;
