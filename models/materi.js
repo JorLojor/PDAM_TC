92570308f332dd0219db0875e3e5560dd16664eb
Rangga Adi Pratama <ranggaadipratama96@yahoo.com>
2023-08-29 09:21:17 +0700

    feat: Materi Multiple Instruktur

--- models/materi.js
+++ models/materi.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const itemsMateriSchema = require('./itemsMateri');
+const itemsMateriSchema = require("./itemsMateri");
 
 const TestSchema = new Schema({
-  pre: { type: mongoose.Schema.Types.ObjectId, ref: 'Test' },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Test' },
-})
+  pre: { type: mongoose.Schema.Types.ObjectId, ref: "Test" },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Test" },
+});
 
-const materiSchema = new Schema({
-  kodeMateri: { type: String, required: true },
-  test: { type: TestSchema, required: false },
-  slug: { type: String, required: true },
-  section: { type: String, required: true }, // Judul
-  description: { type: String, required: true },
-  items: [itemsMateriSchema],
-  instruktur: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
+const materiSchema = new Schema(
+  {
+    kodeMateri: { type: String, required: true },
+    test: { type: TestSchema, required: false },
+    slug: { type: String, required: true },
+    section: { type: String, required: true }, // Judul
+    description: { type: String, required: true },
+    items: [itemsMateriSchema],
+    instruktur: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Materi', materiSchema);
+module.exports = mongoose.model("Materi", materiSchema);
