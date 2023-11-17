1715dd45729bd9e24b01d7bddaa61a6345ef9532
asepdera <asep.derrapurnama@gmail.com>
2023-11-17 08:31:55 +0700

    menambahkan field bio di model users

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new Schema(
     username: { type: String, required: true },
     password: { type: String, required: true },
     instansi: { type: String, required: false },
+    bio: { type: String, required: false },
     nipp: { type: String, required: false },
     userImage: { type: String, required: false },
     role: { type: Number, required: true, default: 3 }, // penentuan user 1 = admin, 2 = instruktur, 3 = student
