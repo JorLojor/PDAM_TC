98aa4566a43c4c47933619693300cc821b3629c4
asepdera <asep.derrapurnama@gmail.com>
2023-11-18 16:09:06 +0700

    menambahkan field kompetensi, bidang, pendidikan di model user

--- models/user.js
+++ models/user.js
@@ -5,11 +5,14 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     instansi: { type: String, required: false },
     bio: { type: String, required: false },
+    pendidikan: [{ type: String, required: false }],
+    kompentensi: [{ type: String, required: false }],
+    bidang: [{ type: String, required: false }],
     nipp: { type: String, required: false },
     userImage: { type: String, required: false },
     role: { type: Number, required: true, default: 3 }, // penentuan user 1 = admin, 2 = instruktur, 3 = student
