d738aee07ed8e76d851896bc0225fc0d56d7aa74
Rangga Adi Pratama <ranggaadipratama96@yahoo.com>
2023-11-28 00:01:54 +0700

    fix: detail hasil evaluasi

--- controllers/evaluationForm.js
+++ controllers/evaluationForm.js
@@ -86,7 +86,7 @@ module.exports = {
     try {
       const { kelas, user } = req.params;
 
-      const result = await EvaluationFormAnswer.findOne({
+      const result = await EvaluationFormAnswer.find({
         kelas,
         $and: [
           {
