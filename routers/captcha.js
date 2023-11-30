d8bd8692e97e059f5e4997e63426dff6a265cf69
Asep Dera Purnama <90121160+asepdera@users.noreply.github.com>
2023-11-30 11:22:51 +0700

    Update captcha.js
    fix request ke req

--- routers/captcha.js
+++ routers/captcha.js
@@ -4,7 +4,7 @@ const axios = require("axios")
 
 router.post('/verify', async (req, res) => {
     try {
-        const { captchaValue } = request.body;
+        const { captchaValue } = req.body;
         const { data } = await axios.post(
             `https://www.google.com/recaptcha/api/siteverify?secret=6LekAx8pAAAAAD56RNNRRGN7WMj_JMRBWqrD5Kgy&response=${captchaValue}`, {}, {
             headers: {
