b1c3ee152a05270924eb54f9774b16f1a2e068dc
Asep Dera Purnama <90121160+asepdera@users.noreply.github.com>
2023-11-30 09:58:34 +0700

    Update captcha.js
    tambah header untuk hindari cors

--- routers/captcha.js
+++ routers/captcha.js
@@ -5,7 +5,11 @@ const axios = require("axios")
 router.post('/verify', async (req, res) => {
     const { captchaValue } = request.body;
     const { data } = await axios.post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=6LekAx8pAAAAAD56RNNRRGN7WMj_JMRBWqrD5Kgy&response=${captchaValue}`
+        `https://www.google.com/recaptcha/api/siteverify?secret=6LekAx8pAAAAAD56RNNRRGN7WMj_JMRBWqrD5Kgy&response=${captchaValue}`,{},{
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+            },
+          },
     );
     res.send(data); 
 })
