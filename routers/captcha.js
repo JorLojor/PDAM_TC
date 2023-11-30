45a13fefc6fc741d5e01ae31570a71d8639e2db4
Asep Dera Purnama <90121160+asepdera@users.noreply.github.com>
2023-11-30 11:18:28 +0700

    Update captcha.js
    coba tracing error pakai try catch

--- routers/captcha.js
+++ routers/captcha.js
@@ -3,15 +3,20 @@ const router = express.Router();
 const axios = require("axios")
 
 router.post('/verify', async (req, res) => {
-    const { captchaValue } = request.body;
-    const { data } = await axios.post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=6LekAx8pAAAAAD56RNNRRGN7WMj_JMRBWqrD5Kgy&response=${captchaValue}`,{},{
+    try {
+        const { captchaValue } = request.body;
+        const { data } = await axios.post(
+            `https://www.google.com/recaptcha/api/siteverify?secret=6LekAx8pAAAAAD56RNNRRGN7WMj_JMRBWqrD5Kgy&response=${captchaValue}`, {}, {
             headers: {
-              "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
             },
-          },
-    );
-    res.send(data); 
+        },
+        );
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error, status: "gagal verifikasi" })
+    }
 })
 
 module.exports = router;
