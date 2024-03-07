108366a65735dfd62c2fa03da512a819d890244c
Rangga Adi Pratama <ranggaadipratama96@yahoo.com>
2024-03-07 14:20:57 +0700

    fix: sertifikat dan get kelas

--- middleware/auth.js
+++ middleware/auth.js
@@ -100,6 +100,25 @@ module.exports = {
     }
   },
 
+  instrukturAndUser: async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const secret_key = process.env.secret_key;
+
+    try {
+      const token = authHeader.split(" ")[1];
+
+      jwt.verify(token, secret_key, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        if (req.user.role !== 3 && req.user.role !== 2)
+          return res.json("you dont have permission").status(403);
+        next();
+      });
+    } catch (error) {
+      res.sendStatus(401);
+    }
+  },
+
   admin: async (req, res, next) => {
     const authHeader = req.headers.authorization;
     const secret_key = process.env.secret_key;
